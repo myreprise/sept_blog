2f5a6513d0a7f39882c1d35d7a1ddb3876827071
myreprise <brettlill@gmail.com>
2015-09-29 00:21:20 +0800

    Initialize repository

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,62 @@
+class ArticlesController < ApplicationController
+
+USER, PASSWORD = 'bbl', 'secret'
+
+before_filter :authentication_check, except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+ 
+  def show
+    @article = Article.find(params[:id])
+  end
+ 
+  def new
+    @article = Article.new
+  end
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
+  def create
+    @article = Article.new(article_params)
+ 
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+ 
+  def update
+    @article = Article.find(params[:id])
+ 
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+ 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+ 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
+
+    def authentication_check
+   authenticate_or_request_with_http_basic do |user, password|
+    user == USER && password == PASSWORD
+   end
+  end
+  
+end
